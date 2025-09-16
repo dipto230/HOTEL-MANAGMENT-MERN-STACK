e5808ec35c68e4481f2d391eef1c07577d253aee
dipto230 <diptoroy230305@gmail.com>
2025-09-16 13:26:15 +0530

    newsletter section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -10,7 +11,8 @@ const Home = () => {
       <Hero />
       <FeaturedDestination />
       <ExclusiveOffers />
-      <Testimonial/>
+      <Testimonial />
+      <NewsLetter/>
     </>
   )
 }
